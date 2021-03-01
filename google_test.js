4a674b3e92e1c44674a763db3e1e0d47377f71b7
The Cat <flameforge@gmail.como>
2021-03-01 12:05:36 +0100

    first commit

---
+++ google_test.js
@@ -0,0 +1,32 @@
+/** @format */
+
+let fs = require('fs');
+
+const webdriver = require('selenium-webdriver'),
+  By = webdriver.By,
+  until = webdriver.until
+
+const test1 = () => {
+  let driver_fx = new webdriver.Builder().forBrowser('firefox').build()
+  searchTest(driver_fx, 'firefox')
+}
+
+const test2 = () => {
+  let driver_chr = new webdriver.Builder().forBrowser('chrome').build()
+  searchTest(driver_chr, 'chrome')
+}
+
+function searchTest(driver, browser) {
+  driver.get('https://www.zeppelin.com/de.html')
+
+    driver.takeScreenshot().then(function (image, err) {
+        fs.writeFile('./tmp/' + browser + '_selenium.png', image, 'base64', function (err) {
+        driver.quit()
+        console.log(err)
+        })
+    })
+
+}
+
+test1();
+test2();
